6d44ee328b5e0cd13bfe129173c8f9723c83c15e
catherinee24 <catherinemendez24@gmail.com>
2023-10-14 13:38:30 -0500

    Foudry course: FOUNDRY-LOTTERY

--- Foundry-Lottery/script/DeployRaffle.s.sol
+++ Foundry-Lottery/script/DeployRaffle.s.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 /**
  * @title A Sample Raffle Smart Contract Deploy
@@ -10,5 +11,28 @@ import {Raffle} from "../src/Raffle.sol";
  * @notice This contract is for creating a sample Raffle Smart Contract Script Deploy.
  */
 contract DeployRaffle is Script {
-    function run() external returns (Raffle) {}
+    function run() external returns (Raffle) {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            uint256 entranceFee,
+            uint256 interval,
+            address vrfCoordinatorV2,
+            bytes32 gasLane,
+            uint64 suscriptionId,
+            uint32 callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            entranceFee,
+            interval,
+            vrfCoordinatorV2,
+            gasLane,
+            suscriptionId,
+            callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        return raffle;
+    }
 }
