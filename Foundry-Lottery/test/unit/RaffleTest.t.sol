a86b2be409b2aafa101bad2c3dea064ee0173b2a
catherinee24 <catherinemendez24@gmail.com>
2023-10-14 14:10:12 -0500

    Foudry course: FOUNDRY-LOTTERY

--- Foundry-Lottery/test/unit/RaffleTest.t.sol
+++ Foundry-Lottery/test/unit/RaffleTest.t.sol
@@ -5,4 +5,21 @@ pragma solidity 0.8.18;
 import {Test, console} from "forge-std/Test.sol";
 import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
 import {Raffle} from "../../src/Raffle.sol";
-contract RaffleTest is Test {}
+contract RaffleTest is Test {
+    /**Variables de los contratos que vamos a deployar */
+    DeployRaffle deployer;
+    Raffle raffle;
+
+    // Creamos un jugador para que interactue con nuestras funciones.
+    address public PLAYER = makeAddr("Player");
+
+    //Distribuimos algo de ether al jugador.
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+
+    /** Funcion setUp(). Todo lo que se escriba en esta funcion sera tomado en cuenta en la funciones de Test que vayamos a crear  */
+    function setUp() external {
+        deployer = new DeployRaffle();
+        raffle = deployer.run();
+
+    }
+}
