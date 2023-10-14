eda05c3bb1cce724d878e654bf3e16b8441d645e
catherinee24 <catherinemendez24@gmail.com>
2023-10-14 14:36:52 -0500

    Foudry course: FOUNDRY-LOTTERY

--- Foundry-Lottery/test/unit/RaffleTest.t.sol
+++ Foundry-Lottery/test/unit/RaffleTest.t.sol
@@ -6,23 +6,39 @@ import {Test, console} from "forge-std/Test.sol";
 import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
 import {Raffle} from "../../src/Raffle.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
 contract RaffleTest is Test {
-    /**Variables de los contratos que vamos a deployar */
+    /**
+     * Variables de los contratos que vamos a deployar
+     */
     DeployRaffle deployer;
     Raffle raffle;
-    HelperConfig heplperConfig;
+    HelperConfig helperConfig;
 
     // Creamos un jugador para que interactue con nuestras funciones.
     address public PLAYER = makeAddr("Player");
-
-    //Distribuimos algo de ether al jugador.
     uint256 public constant STARTING_USER_BALANCE = 10 ether;
 
-    /** Funcion setUp(). Todo lo que se escriba en esta funcion sera tomado en cuenta en la funciones de Test que vayamos a crear  */
+    uint256 entranceFee;
+    uint256 interval;
+    address vrfCoordinatorV2;
+    bytes32 gasLane;
+    uint64 suscriptionId;
+    uint32 callbackGasLimit;
+
+    /**
+     * Funcion setUp(). Todo lo que se escriba en esta funcion sera tomado en cuenta en la funciones de Test que vayamos a crear
+     */
     function setUp() external {
         deployer = new DeployRaffle();
-        (raffle, heplperConfig) = deployer.run();
-     
+        (raffle, helperConfig) = deployer.run();
 
+        (entranceFee, interval, vrfCoordinatorV2, gasLane, suscriptionId, callbackGasLimit) =
+            helperConfig.activeNetworkConfig();
+    }
+
+    function testRaffleInitializesInOpenState() public view {
+        // Escribimos Raffle.RaffleState.OPEN diciendo: en el contrato `Raffle` encuentra un type `RaffleState` y que este OPEN.
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
     }
 }
