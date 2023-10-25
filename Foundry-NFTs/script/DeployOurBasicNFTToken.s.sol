134d5ff4488e36f70d63acee93352d104ab38d6e
catherinee24 <catherinemendez24@gmail.com>
2023-10-25 17:20:12 -0500

    Foudry course: FOUNDRY-NFTs

---
+++ Foundry-NFTs/script/DeployOurBasicNFTToken.s.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {OurBasicNFTToken} from "../src/OurBasicNFTToken.sol";
+
+contract DeployOurBasicNFTToken is Script {
+    function run() external returns (OurBasicNFTToken) {
+        vm.startBroadcast();
+        OurBasicNFTToken nftToken = new OurBasicNFTToken();
+        vm.stopBroadcast();
+        return nftToken;
+    }
+}
--- Foundry-NFTs/src/OurBasicNFTToken.sol
+++ Foundry-NFTs/src/OurBasicNFTToken.sol
@@ -6,12 +6,22 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract OurBasicNFTToken is ERC721 {
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
 
     constructor() ERC721("MavericksDoggies", "MDGS") {
         s_tokenCounter = 0;
     }
 
+    /// @dev _safeMint funcion de la implentacion de OZ ERC721.sol
+    function mint(string memory _tokenURI) public {
+        s_tokenIdToUri[s_tokenCounter] = _tokenURI;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
     function tokenURI(
         uint256 _tokenId
-    ) public view override returns (string memory) {}
+    ) public view override returns (string memory) {
+        return s_tokenIdToUri[_tokenId];
+    }
 }
