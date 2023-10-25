770fd94bcba36cdc0b0ee128dd852a2638c03c1f
catherinee24 <catherinemendez24@gmail.com>
2023-10-25 15:58:10 -0500

    Foudry course: FOUNDRY-NFTs

--- Foundry-NFTs/src/OurBasicNFTToken.sol
+++ Foundry-NFTs/src/OurBasicNFTToken.sol
@@ -6,7 +6,12 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract OurBasicNFTToken is ERC721 {
     uint256 private s_tokenCounter;
+
     constructor() ERC721("MavericksDoggies", "MDGS") {
         s_tokenCounter = 0;
     }
+
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory) {}
 }
