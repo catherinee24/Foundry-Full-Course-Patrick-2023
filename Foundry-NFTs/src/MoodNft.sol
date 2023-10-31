960512753ad3e9fed0178a3f11cc0b9ac365c9b9
catherinee24 <catherinemendez24@gmail.com>
2023-10-30 19:02:18 -0500

    Foudry course: FOUNDRY-NFTs

--- Foundry-NFTs/src/MoodNft.sol
+++ Foundry-NFTs/src/MoodNft.sol
@@ -5,13 +5,15 @@ pragma solidity ^0.8.19;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/base64.sol";
 
-
 contract MoodNft is ERC721 {
     uint256 private s_tokenCounter;
     string private s_sadSvgImageURI;
     string private s_happySvgImageURI;
 
-    constructor(string memory _sadSvgImageURI, string memory _happySvgImageURI) ERC721("Mood Nft", "MN") {
+    constructor(
+        string memory _sadSvgImageURI,
+        string memory _happySvgImageURI
+    ) ERC721("Mood Nft", "MN") {
         s_tokenCounter = 0;
         s_sadSvgImageURI = _sadSvgImageURI;
         s_happySvgImageURI = _happySvgImageURI;
@@ -22,7 +24,16 @@ contract MoodNft is ERC721 {
         s_tokenCounter++;
     }
 
-    function tokenURI(uint256 _tokenId) public view override returns(string memory){
-
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory) {
+        //Este va a ser el Metadata de nuestro Token.
+        string memory tokenMetadata = string.concat(
+            '{"name": "',
+            name(),
+            '", "description": "An Nft that reflects the owners mood.", "attributes": [{"trait_type": "moodiness", "value": 100}], "image": "',
+            imageURI,
+            '"}'
+        );
     }
 }
