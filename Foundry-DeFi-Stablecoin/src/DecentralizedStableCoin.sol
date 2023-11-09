d7480f1b13c085d9425b03c9ea1e6e95ffad8b69
catherinee24 <catherinemendez24@gmail.com>
2023-11-09 18:38:35 -0500

    Foundry course: FOUNDRY-DEFI-STABLECOIN

--- Foundry-DeFi-Stablecoin/src/DecentralizedStableCoin.sol
+++ Foundry-DeFi-Stablecoin/src/DecentralizedStableCoin.sol
@@ -6,13 +6,15 @@ import { ERC20Burnable, ERC20 } from "@openzeppelin/contracts/token/ERC20/extens
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title DecentralizedStableCoin
-/// @author Catherine Maverick
-/// @notice This is the contract meant to be governed by the CSCEngine contract. This contract is just the    ERC-20
-/// implementation of our stablecoin system.
+/// @author Catherine Maverick from catellatech.
+/// @notice This is the contract meant to be governed by the CSCEngine contract. This contract is just the    ERC-20 implementation of our stablecoin system.
 /// @dev Collateral: Exogenous (ETH & BTC)
 /// @dev Relative Stability: Pegged to USD
 
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    /*//////////////////////////////////////////////////////////////
+                                ERRORS
+    //////////////////////////////////////////////////////////////*/
     error DecentralizedStableCoin__MustBeMoreThanZero();
     error DecentralizedStableCoin__BurnAmountExeedsBalance();
     error DecentralizedStableCoin__NotAddressZero();
@@ -21,14 +23,9 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     constructor() ERC20("CatellaUSD", "CSC") { }
 
     /**
-     * @notice Funcion burn(): Diseñada para permitir que el propietario del contrato (CSCEnginee) queme una cierta
-     * cantidad de tokens, siempre y cuando cumpla con las verificaciones de límite y tenga suficientes tokens en su
-     * saldo.
-     * @dev if() statement 1: Verifica si la cantidad que se quiere quemar es menor o igual a cero. Si es así, revierte la transacción
-     * con un mensaje de error indicando que la cantidad debe ser mayor que cero.
-     * @dev if() statement 2: Verifica si la cantidad que se quiere quemar es mayor que el saldo disponible en la cuenta del
-     * propietario. Si es así, revierte la transacción con un mensaje de error indicando que la cantidad a quemar
-     * excede el saldo.
+     * @notice Funcion burn(): Diseñada para permitir que el propietario del contrato (CSCEnginee) queme una cierta cantidad de tokens, siempre y cuando cumpla con las verificaciones de límite y tenga suficientes tokens en su saldo.
+     * @dev if() statement 1: Verifica si la cantidad que se quiere quemar es menor o igual a cero. Si es así, revierte la transacción con un mensaje de error indicando que la cantidad debe ser mayor que cero.
+     * @dev if() statement 2: Verifica si la cantidad que se quiere quemar es mayor que el saldo disponible en la cuenta del propietario. Si es así, revierte la transacción con un mensaje de error indicando que la cantidad a quema excede el saldo.
      * @param _amount Cantidad de tokens que se quieren quemar.
      */
     function burn(uint256 _amount) public override onlyOwner {
@@ -39,14 +36,16 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     }
 
     /**
-     * @notice El balance de la persona que llama debe ser mayor a la cantidad que quiere quemar.
-     * @param _to Dirección a la cual se van a enviar las nuevas unidades de Token creados.
-     * @param _amount Cantidad de nuevas unidades de Tokens que se van a crear y enviar a la dirección especificada en
-     * "_to".
+     * @notice Funcion mint(): Diseñada para permitir que el propietario del contrato (CSCEnginee) cree nuevas unidades de tokens y las asigne a una dirección específica, siempre y cuando se cumplan las condiciones de validación.
+     * @dev if() statement 1: Verifica si la dirección de destino (_to) es la dirección 0. Revierte la transacción si es así, con un mensaje de error indicando que la dirección no puede ser la dirección 0.
+     * @dev if() statement 2: Verifica si la cantidad de tokens que se quiere crear y asignar es menor o igual a cero. Revierte la transacción si es así, con un mensaje de error indicando que la cantidad debe ser mayor que cero.
+     * @param _amount Cantidad de nuevas unidades de Tokens que se van a crear y enviar a la dirección especificada en "_to".
+     * @param _to Dirección a la cual se van a enviar las nuevas unidades de tokens creadas.
+     * @return Devuelve true para indicar que la operación de minting fue exitosa.
      */
     function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
-        if(_to == address(0)) revert DecentralizedStableCoin__NotAddressZero();
-        if(_amount <= 0) revert DecentralizedStableCoin__AmountMustBeGreaterThanZero();
+        if (_to == address(0)) revert DecentralizedStableCoin__NotAddressZero();
+        if (_amount <= 0) revert DecentralizedStableCoin__AmountMustBeGreaterThanZero();
         _mint(_to, _amount);
         return true;
     }
