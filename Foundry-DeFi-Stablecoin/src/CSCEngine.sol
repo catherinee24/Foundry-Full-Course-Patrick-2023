f96b1cae29a2ca372f84830fd5739199788c16ab
catherinee24 <catherinemendez24@gmail.com>
2023-11-10 18:39:34 -0500

    Foundry course: FOUNDRY-DEFI-STABLECOIN

--- Foundry-DeFi-Stablecoin/src/CSCEngine.sol
+++ Foundry-DeFi-Stablecoin/src/CSCEngine.sol
@@ -29,6 +29,8 @@ contract CSCEngine is ReentrancyGuard {
     error CSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
     error CSCEngine__TokenNotAllowed();
     error CSCEngine__TransferFaild();
+    error CSCEngine__BreaksHealthFactor(uint256 healthFactor);
+    error CSCEngine__MintFaild();
 
     /*/////////////////////////////////////////////////////////////////////////////////////////////////////////
                                                 STATE VARIABLES 
@@ -37,6 +39,7 @@ contract CSCEngine is ReentrancyGuard {
     uint256 private constant PRECISION = 1e18;
     uint256 private constant LIQUIDATION_THRESHOLD = 50; // 200% Overcollateralized
     uint256 private constant LIQUIDATION_PRECISION = 100;
+    uint256 private constant MIN_HEALTH_FACTOR = 1;
 
     mapping(address token => address priceFeed) private s_priceFeeds;
     mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
@@ -109,14 +112,20 @@ contract CSCEngine is ReentrancyGuard {
     function redeemCollateral() external { }
 
     /**
+     * @notice Esta función permite a los usuarios crear nuevos tokens CSC, siempre y cuando la cantidad sea mayor que
+     * cero, se respeten las reglas de no recursividad y Health Factor del usuario esté dentro de ciertos límites.
+     * Además, registra el aumento de tokens emitidos y revierte la transacción si la creación de tokens no se
+     * realiza con éxito.
      * @notice Sigue el patrón CEI
      * @notice Se debe tener más valor del collateral que el minimo threshold.
-     * @param _amountCscToMint Cantida de CSC (Catella StableCoin) que el usuario quiere mintear.
+     * @param _amountCscToMint La cantidad de tokens CSC que se van a crear o "emitir".
      *
      */
     function mintCsc(uint256 _amountCscToMint) external moreThanZero(_amountCscToMint) nonReentrant {
         s_CSCMinted[msg.sender] += _amountCscToMint;
         _revertIfHealthFactorIsBroken(msg.sender);
+        bool minted = i_cscToken.mint(msg.sender, _amountCscToMint);
+        if (!minted) revert CSCEngine__MintFaild();
     }
 
     function burnCsc() external { }
@@ -143,29 +152,42 @@ contract CSCEngine is ReentrancyGuard {
     }
 
     /**
-     * @return Retorna que tan cerca de la liquidación está un usuario.
-     * @notice Si un usuario se va a bajo de 1, entonces pueden se liquidado.
+     * @notice  Funcion diseñada para calcular un factor de salud financiera (health Factor) de un usuario
+     * @notice Si un usuario se va a bajo de 1(MIN_HEALTH_FACTOR), entonces puede ser liquidado.
+     * @notice Ajusta el valor del colateral del usuario en función de un umbral de liquidación (Threshold) (utilizado
+     * para establecer el límite mínimo aceptable de colateralización).
+     * @param _user  La dirección del usuario para la cual se está calculando el factor de salud financiera.
+     * @return Calcula y devuelve el factor de salud financiera (Helath Factor). Este cálculo involucra una comparación
+     * entre el valor del colateral ajustado y la cantidad total de tokens CSC creados por el usuario.(Retorna que tan
+     * cerca de la liquidación está un usuario).
      */
     function _healthFactor(address _user) private view returns (uint256) {
         (uint256 totalCscMinted, uint256 collateralValueInUSD) = _getAccountInformation(_user);
         uint256 collateralAdjustedForThreshold = (collateralValueInUSD * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
-        return(collateralAdjustedForThreshold * PRECISION) / totalCscMinted;
+        return (collateralAdjustedForThreshold * PRECISION) / totalCscMinted;
     }
 
     /**
-     *
-     * @param _user Direccion del usuario.
+     * @notice Función diseñada para verificar si el factor de salud financiera de un usuario cae por debajo de un
+     * cierto límite establecido.
+     * @param _user  La dirección del usuario para el cual se está verificando el factor de salud financiera.
      */
-    function _revertIfHealthFactorIsBroken(address _user) internal view { }
+    function _revertIfHealthFactorIsBroken(address _user) internal view {
+        uint256 userHealthFactor = _healthFactor(_user);
+        if (userHealthFactor < MIN_HEALTH_FACTOR) revert CSCEngine__BreaksHealthFactor(userHealthFactor);
+    }
 
     /*/////////////////////////////////////////////////////////////////////////////////////////////////////////
                                         PUBLIC & EXTERNAL VIEW FUNCTIONS
     /////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 
     /**
-     * @notice Para obtener el valor tenemos que hacer un loop a través de cada collateral token, obtener el valor que
-     * se ha depositado y mapearlo al precio para obtener el valor de USD.
-     * @param _user
+     * @notice Esta función tiene la finalidad de calcular el valor total en dólares estadounidenses (USD) de todo el
+     * colateral depositado por un usuario
+     * @notice Para obtener el valor tenemos que hacer un loop a través de cada (collateral token), obtener el valor
+     * que se ha depositado y mapearlo al precio para obtener el valor de USD.
+     * @param _user La dirección del usuario para el que se está calculando el valor del colateral en USD.
+     * @return Retorna el valor total del colateral del usuario en dólares estadounidenses.
      */
     function getAccountCollateralValueInUSD(address _user) public view returns (uint256 totalCollateralValueInUSD) {
         for (uint256 i = 0; i < s_collateralTokens.length; i++) {
@@ -177,6 +199,13 @@ contract CSCEngine is ReentrancyGuard {
     }
 
     /**
+     * @notice Esta función tiene como objetivo calcular el valor en dólares estadounidenses (USD) de una cierta
+     * cantidad de un token específico.
+     * @param _token La dirección del token del cual se está calculando el valor en USD.
+     * @param _amount La cantidad del token para la que se quiere determinar el valor en USD.
+     * @notice Se obtiene el precio del token en USD mediante un Price Feed (ChainLink) vinculado a ese token en
+     * particular, utilizando la interfaz `AggregatorV3Interface`. Se accede a los datos más recientes del feed de
+     * precios para obtener el valor del token en USD.
      * @notice 1 ETH = $1000
      * @notice El valor retornado por el CL será 1000 * 1e8
      */
