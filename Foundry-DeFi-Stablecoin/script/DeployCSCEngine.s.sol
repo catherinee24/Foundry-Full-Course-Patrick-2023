50442d78abc60bc65219958ccddb6e4fbd523a9c
catherinee24 <catherinemendez24@gmail.com>
2023-11-11 15:47:47 -0500

    Foundry course: FOUNDRY-DEFI-STABLECOIN

--- Foundry-DeFi-Stablecoin/script/DeployCSCEngine.s.sol
+++ Foundry-DeFi-Stablecoin/script/DeployCSCEngine.s.sol
@@ -1,19 +1,19 @@
-// SPDX-License-Identifier: MIT
+// // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+// pragma solidity ^0.8.18;
 
-import { Script } from "forge-std/Script.sol";
-import { CSCEngine } from "../src/CSCEngine.sol";
-import { DecentralizedStableCoin } from "../src/DecentralizedStableCoin.sol";
+// import { Script } from "forge-std/Script.sol";
+// import { CSCEngine } from "../src/CSCEngine.sol";
+// import { DecentralizedStableCoin } from "../src/DecentralizedStableCoin.sol";
 
-contract DeployCSCEngine is Script {
-    CSCEngine cscEngine;
-    DecentralizedStableCoin decentralizedStableCoin;
+// contract DeployCSCEngine is Script {
+//     CSCEngine cscEngine;
+//     DecentralizedStableCoin decentralizedStableCoin;
 
-    function run() external returns (DecentralizeStableCoin, CSCEngine) {
-        vm.startBroadcast();
-        decentralizedStableCoin = new DecentralizedStableCoin();
-        cscEngine = new CSCEngine();
-        vm.stopBroadcast();
-    }
-}
+//     function run() external returns (DecentralizedStableCoin, CSCEngine) {
+//         vm.startBroadcast();
+//         decentralizedStableCoin = new DecentralizedStableCoin();
+//         cscEngine = new CSCEngine();
+//         vm.stopBroadcast();
+//     }
+// }
--- Foundry-DeFi-Stablecoin/script/HelperConfig.s.sol
+++ Foundry-DeFi-Stablecoin/script/HelperConfig.s.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.18;
 import { Script } from "forge-std/Script.sol";
 import { CSCEngine } from "../src/CSCEngine.sol";
 import { DecentralizedStableCoin } from "../src/DecentralizedStableCoin.sol";
-import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
-import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import { MockV3Aggregator } from "../test/mocks/MockV3Aggregator.sol";
+import { ERC20Mock } from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -17,6 +17,10 @@ contract HelperConfig is Script {
     }
 
     NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 1000e8;
 
     constructor() { }
 
@@ -30,11 +34,24 @@ contract HelperConfig is Script {
         });
     }
 
-    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory){
-        if(activeNetworkConfig.wethUsdPriceFeed != address(0)){
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.wethUsdPriceFeed != address(0)) {
             return activeNetworkConfig;
         }
-        
-         
+
+        vm.startBroadcast();
+        MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
+        ERC20Mock wETHMock = new ERC20Mock();
+
+        MockV3Aggregator btcUsdPriceFeed = new MockV3Aggregator(DECIMALS, BTC_USD_PRICE);
+        ERC20Mock wBTCMock = new ERC20Mock();
+        vm.stopBroadcast();
+
+        return NetworkConfig({
+            wethUsdPriceFeed: address(ethUsdPriceFeed),
+            wbtcUsdPriceFeed: address(btcUsdPriceFeed),
+            weth: address(wETHMock),
+            wbtc: address(wBTCMock)
+        });
     }
 }
