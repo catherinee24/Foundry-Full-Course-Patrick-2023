fa7c826655b16435ec8dc0759450fc301a5b35ab
catherinee24 <catherinemendez24@gmail.com>
2023-11-11 15:58:37 -0500

    Foundry course: FOUNDRY-DEFI-STABLECOIN

--- Foundry-DeFi-Stablecoin/script/HelperConfig.s.sol
+++ Foundry-DeFi-Stablecoin/script/HelperConfig.s.sol
@@ -8,6 +8,15 @@ import { MockV3Aggregator } from "../test/mocks/MockV3Aggregator.sol";
 import { ERC20Mock } from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 1000e8;
+
+    uint256 public constant DEFAULT_ANVIL_PRIVATE_KEY =
+        0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
     struct NetworkConfig {
         address wethUsdPriceFeed;
         address wbtcUsdPriceFeed;
@@ -16,13 +25,13 @@ contract HelperConfig is Script {
         uint256 deployerKey;
     }
 
-    NetworkConfig public activeNetworkConfig;
-
-    uint8 public constant DECIMALS = 8;
-    int256 public constant ETH_USD_PRICE = 2000e8;
-    int256 public constant BTC_USD_PRICE = 1000e8;
-
-    constructor() { }
+    constructor() {
+        if (block.chainid == 11_155_111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
 
     function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
         return NetworkConfig({
@@ -51,7 +60,8 @@ contract HelperConfig is Script {
             wethUsdPriceFeed: address(ethUsdPriceFeed),
             wbtcUsdPriceFeed: address(btcUsdPriceFeed),
             weth: address(wETHMock),
-            wbtc: address(wBTCMock)
+            wbtc: address(wBTCMock),
+            deployerKey: DEFAULT_ANVIL_PRIVATE_KEY
         });
     }
 }
