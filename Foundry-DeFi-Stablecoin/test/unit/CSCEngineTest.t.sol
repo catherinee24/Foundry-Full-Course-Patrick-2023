82d4a0beabcedcef3dcc1c943fb81951b8aa4505
catherinee24 <catherinemendez24@gmail.com>
2023-11-12 15:37:02 -0500

    Foundry course: FOUNDRY-DEFI-STABLECOIN

--- Foundry-DeFi-Stablecoin/test/unit/CSCEngineTest.t.sol
+++ Foundry-DeFi-Stablecoin/test/unit/CSCEngineTest.t.sol
@@ -2,6 +2,19 @@
 
 pragma solidity ^0.8.18;
 
-import { Test } from "forge-std/Test.sol";
+import { Test, console } from "forge-std/Test.sol";
+import { DeployCSCEngine } from "../../script/DeployCSCEngine.s.sol";
+import { DecentralizedStableCoin } from "../../src/DecentralizedStableCoin.sol";
+import { CSCEngine } from "../../src/DecentralizedStableCoin.sol";
 
-contract CSCEngineTest is Test { }
+contract CSCEngineTest is Test {
+    DeployCSCEngine deployer;
+    DecentralizedStableCoin csc;
+    CSCEngine cscEngine;
+
+    function setUp() public {
+        deployer = new DeployCSCEngine();
+        csc = new DecentralizedStableCoin();
+        cscEngine = deployer.run();
+    }
+}
