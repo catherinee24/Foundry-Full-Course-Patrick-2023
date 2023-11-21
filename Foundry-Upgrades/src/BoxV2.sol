7249efe18b1b2250f7dfeb1c4494b093a731e588
catherinee24 <catherinemendez24@gmail.com>
2023-11-21 15:58:42 -0500

    Foundry course: FOUNDRY-UPGRADES

--- Foundry-Upgrades/src/BoxV2.sol
+++ Foundry-Upgrades/src/BoxV2.sol
@@ -2,14 +2,18 @@
 
 pragma solidity ^0.8.19;
 
-contract BoxV1 {
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable@5.0.0/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV2 {
     uint256 internal number;
+
+    function setNumber(uint256 _number) external { }
 
     function getNumber() external view returns (uint256) {
         return number;
     }
 
     function version() external pure returns (uint256) {
-        return 1;
+        return 2;
     }
 }
