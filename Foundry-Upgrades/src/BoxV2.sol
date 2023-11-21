32e2b0e820ef955a808310d16510aadd57e24ef2
catherinee24 <catherinemendez24@gmail.com>
2023-11-21 16:40:20 -0500

    Foundry course: FOUNDRY-UPGRADES

--- Foundry-Upgrades/src/BoxV2.sol
+++ Foundry-Upgrades/src/BoxV2.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
-import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract BoxV2 {
     uint256 internal number;
