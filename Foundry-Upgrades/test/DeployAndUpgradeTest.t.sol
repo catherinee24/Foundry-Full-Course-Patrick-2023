bc388d8d485a418fa0ae8b30e63d8a0893386117
catherinee24 <catherinemendez24@gmail.com>
2023-11-22 15:52:38 -0500

    Foundry course: FOUNDRY-UPGRADES

--- Foundry-Upgrades/test/DeployAndUpgradeTest.t.sol
+++ Foundry-Upgrades/test/DeployAndUpgradeTest.t.sol
@@ -8,6 +8,15 @@ import {BoxV2} from "../src/BoxV2.sol";
 import {DeployBox} from "../script/DeployBox.s.sol";
 import {UpgradeBox} from "../script/UpgradeBox.s.sol";
 contract DeployAndUpgradeTest is Test {
-    
-    function setUp() public {}
+    DeployBox public deployer;
+    UpgradeBox public upgrader;
+    addrees public proxy;
+    address public OWNER = makeAddr("owner");
+    function setUp() public {
+        deployer = new DeployBox();
+        upgrader = new UpgradeBox();
+        proxy = new deployer.run(); //El proxy ahora mismo, apunta a BoxV1.
+    }
+
+    function testUpgrades() public {}
 }
