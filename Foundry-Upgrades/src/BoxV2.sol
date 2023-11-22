dfab87872f775ca3347d20977326e9c2f7e8a910
catherinee24 <catherinemendez24@gmail.com>
2023-11-22 12:24:53 -0500

    Foundry course: FOUNDRY-UPGRADES

--- Foundry-Upgrades/src/BoxV2.sol
+++ Foundry-Upgrades/src/BoxV2.sol
@@ -2,6 +2,10 @@
 
 pragma solidity ^0.8.18;
 
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
 contract BoxV2 {
     uint256 internal number;
 
