45aeee18e6dc78977018b4689b1773057605ddf2
catherinee24 <catherinemendez24@gmail.com>
2023-11-24 16:57:21 -0500

    Foundry course: FOUNDRY-DAOs-GOVERNANCE

--- Foundry-Daos-Governance/test/MyGovernorTest.t.sol
+++ Foundry-Daos-Governance/test/MyGovernorTest.t.sol
@@ -22,17 +22,20 @@ contract MyGovernorTest is Test {
     address[] targets;
 
     address VOTER = address(1);
+
     uint256 public constant INITIAL_SUPPLY = 100 ether;
-    uint256 public constant MIN_DELAY = 3600 seconds; //1 hora: después de que una votación sea aprobada, tienes 1 hora antes de poder votar.
-    uint256 public constant VOTING_DELAY= 1; // Representa la cantidad de un bloque hasta que la votación esté activa.
+    uint256 public constant MIN_DELAY = 3600 seconds; //1 hora: después de que una votación sea aprobada, tienes 1
+        // hora antes de poder votar.
+    uint256 public constant VOTING_DELAY = 1; // Representa la cantidad de un bloque hasta que la votación esté activa.
+    uint256 public constant VOTIN_PERIOD = 50_400; // 1 semana
 
     // Le minteamos algo de tokens a VOTER
     //llamamos a la funcion delegate de el archivo Votes.sol heredado por GovernanceToken, para delegarle los tokens
-    // a nosotros mismos 
+    // a nosotros mismos
     function setUp() public {
         governanceToken = new GovernanceToken();
         governanceToken.mint(VOTER, INITIAL_SUPPLY);
-        
+
         vm.startPrank(VOTER);
         governanceToken.delegate(VOTER);
 
@@ -62,36 +65,67 @@ contract MyGovernorTest is Test {
         box.store(1);
     }
 
-/** Governor.sol by Openzeppelin
-    function propose(
-        address[] memory targets,
-        uint256[] memory values,
-        bytes[] memory calldatas,
-        string memory description
-    ) public virtual returns (uint256) {} 
-*/
+    /**
+     * Governor.sol by Openzeppelin
+     * function propose(
+     *     address[] memory targets,
+     *     uint256[] memory values,
+     *     bytes[] memory calldatas,
+     *     string memory description
+     * ) public virtual returns (uint256) {}
+     */
     function testGovernanceUpdateBox() public {
         uint256 valueStore = 28;
         string memory description = "Store 1 in Box";
         bytes memory encodedFunctionCall = abi.encodeWithSignature("store(uint256)", valueStore);
-        
+
         values.push(0);
         calldatas.push(encodedFunctionCall);
         targets.push(address(box));
 
-        // 1. propuesta a la DAO.
+        /**
+         * 1. Hacemos la propuesta a la DAO.
+         */
         uint256 proposalId = myGovernor.propose(targets, values, calldatas, description);
 
         // Podemos ver el estado de la propuesta. La propuesta estará pending.
         console.log("Proposal State: ", uint256(myGovernor.state(proposalId)));
 
-        vm.warp(block.timestamp + VOTING_DELAY +1);
+        vm.warp(block.timestamp + VOTING_DELAY + 1);
         vm.roll(block.number + VOTING_DELAY + 1);
 
         // Podemos ver el estado de la propuesta. La propuesta estará active.
         console.log("Proposal State: ", uint256(myGovernor.state(proposalId)));
 
-        // Podemos empezar a votar.
+        /**
+         *  2.Podemos empezar a votar.
+         */
+        string memory reason = "Because i wanna be a better dev and security researcher";
 
+        // Para votar necesitamos poner por qué estamos votando, las opciones son:
+        //Againts, for, abstain.
+        // vamos a votar for, y for esta en el index 1.
+        uint8 voteWay = 1;
+        vm.startPrank(VOTER);
+        myGovernor.castVoteWithReason(proposalId, voteWay, reason);
+
+        vm.warp(block.timestamp + VOTIN_PERIOD + 1);
+        vm.roll(block.number + VOTIN_PERIOD + 1);
+
+        /**
+         * 3. "Queue the txs"/poner en cola las transacciones
+         * GovernorTomelockControl.sol by openzeppelin
+         * function _queueOperations(
+         *     uint256 proposalId,
+         *     address[] memory targets,
+         *     uint256[] memory values,
+         *     bytes[] memory calldatas,
+         *     bytes32 descriptionHash
+         * ) internal virtual override returns (uint48) {
+         */
+
+        /**
+         * 4. Ejecutar la txs
+         */
     }
 }
